d976fa070c38805c0af0d91aabc77fd975ae730c
Kamilou 52 <elkemal2000@yahoo.fr>
2023-03-13 16:26:36 +0100

    Let execute the conversion formulas.

---
+++ converter.js
@@ -0,0 +1,30 @@
+// Func: Gn$ to CHF
+// Gn$ * 0.97
+function Gn$ToCHF(Gn$){
+    return Gn$ * 0.97;
+}
+
+// func: Celsius to Farenheit
+// (celsius * 9/5) + 32
+function celsiusToFarenheit(celsius){
+    (celsius * 9/5) + 32
+}
+
+// Func: Liters to Gallons
+// liters / 3.785
+function litersToGallons(liters) {
+    return liters / 3.785;
+}
+
+
+// func: Ask if the user wants to convert something
+
+// Func: Ask the type of conversion
+
+// Func: Guess user to enter a value
+
+// Main:
+// -continue while the user wants to convert something
+// -Check which conversion
+// -Get the value
+// -Display the result
